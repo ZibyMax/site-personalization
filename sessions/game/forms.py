1b1e6578ff478642c08a4b3b5103ae8d40df0d14
ZibyMax <max@rekstar-nn.ru>
2019-05-21 09:10:12 +0300

    test + название поля

--- sessions/game/forms.py
+++ sessions/game/forms.py
@@ -3,4 +3,4 @@ from .views import MIN_VALUE, MAX_VALUE
 
 
 class GameForm(forms.Form):
-    value = forms.IntegerField(min_value=MIN_VALUE, max_value=MAX_VALUE)
+    value = forms.IntegerField(min_value=MIN_VALUE, max_value=MAX_VALUE, label='Введите число')
